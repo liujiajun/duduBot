beaa4245aab6288b79d3bad486deb9bbceb7113e
Jiajun <liujiajun1999@gmail.com>
2019-07-11 15:08:57 +0700

    add Secret Crush feature

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,9 +1,12 @@
 import json
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 from DialogflowClient import DialogflowClient
 import logging
 
-
+FINISH_ADDING = 0
+ADD_CRUSH = 1
+REMOVE_CRUSH = 2
 class TelegramBot:
     def __init__(self):
         with open('bot_config_private.json') as f:  # Use bot_config_private.json for development
@@ -16,6 +19,9 @@ class TelegramBot:
         text_handler = MessageHandler(Filters.text, self._receiveText)
         self.dispatcher.add_handler(text_handler)
 
+        crush_handler = SecretCrush().convHandler
+        self.dispatcher.add_handler(crush_handler)
+
         logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                             level=logging.INFO)
 
@@ -23,10 +29,47 @@ class TelegramBot:
         self.updater.start_polling()
 
     def _receiveText(self, bot, update):
-        print(update.message.chat_id)
         bot.send_chat_action(chat_id=update.message.chat_id, action='TYPING')
         reply = self.dfClient.detectIntect(update.message.chat_id, update.message.text)
         bot.send_message(chat_id=update.message.chat_id, text=reply)
 
-    def _receiveImage(self, update, context):
-        context.bot.send_message(chat_id=update.message.chat_id, text='I received an image')
+    def _receiveCrush(self, bot, update):
+        bot.send_chat_action(chat_id=update.message.chat_id, action='TYPING')
+        bot.send_message(chat_id=update.message.chat_id, text='Dududu')
+
+class SecretCrush:
+    def __init__(self):
+        self.convHandler = ConversationHandler(
+            entry_points=[CommandHandler('ilike', self.startCrush)],
+
+            states = {
+                REMOVE_CRUSH: [MessageHandler(Filters.text, self.deleteCrush)],
+                FINISH_ADDING: [MessageHandler(Filters.text, self.finishAdding)]
+            },
+
+            fallbacks=[CommandHandler('cancel', self.cancel)]
+
+        )
+    def startCrush(self, bot, update):
+        reply_keyboard = [['I want to remove a crush']]
+        update.message.reply_text("Hi! I'm Dudu, your wingman in relationship. "
+                                  "\nNow, tell me the name of your SECRET CRUSH. Don't worry. I won't gossip.",
+                                  reply_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+                                  )
+        return FINISH_ADDING
+
+    def finishAdding(self, bot, update):
+        print('finishAdding', update.message.text)
+        update.message.reply_text("Got it! I will notify you and your crush immediately once there's a match. "
+                                  "Meanwhile, you can edit your crush list anytime. All the best!",
+                                  reply_markup=ReplyKeyboardRemove())
+        return ConversationHandler.END
+
+    def deleteCrush(self, bot, update):
+        update.message.reply_text('Finished')
+        return ConversationHandler.END
+
+    def cancel(self, bot, update):
+        update.message.reply_text('Bye! I hope we can talk again some day.',
+                                  reply_markup=ReplyKeyboardRemove())
+        return ConversationHandler.END
